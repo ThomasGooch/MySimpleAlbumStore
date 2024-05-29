70385b69e5faca5dd30f97efa620854a557d56df
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-05-29 16:29:09 -0400

    added models and starting mediatr abstractions

---
+++ src/MySimpleAlbumStore.API/Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace MySimpleAlbumStore.API.Models
+{
+    public class Album
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public List<Artist> Artist { get; set; } = [];
+        public string ImageUrl { get; set; } = string.Empty;
+
+    }
+}
---
+++ src/MySimpleAlbumStore.API/Models/Artist.cs
@@ -0,0 +1,8 @@
+﻿namespace MySimpleAlbumStore.API.Models
+{
+    public class Artist
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
