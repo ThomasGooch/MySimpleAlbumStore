ea7f11676c656237fe9a24858ff50c410c1cee03
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-05-30 15:09:40 -0400

    created basic unit test, add mapster, created endpoint

---
+++ src/MySimpleAlbumStore.API/Artists/GetArtists/GetArtistsEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Mapster;
+
+namespace MySimpleAlbumStore.API.Artists.GetArtists
+{
+
+    // public record GetArtistsRequest();
+    public record GetArtistsResponse(List<Artist> Artists);
+
+    public class GetArtistsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/artists", async (ISender sender, CancellationToken cancellationToken) =>
+            {
+                var result = await sender.Send(new GetArtistsQuery(), cancellationToken);
+                var response = result.Adapt<GetArtistsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetArtists")
+            .Produces<GetArtistsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Artists")
+            .WithDescription("Get Artists");
+        }
+    }
+}
--- src/MySimpleAlbumStore.API/Program.cs
+++ src/MySimpleAlbumStore.API/Program.cs
@@ -1,16 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // configure services
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
     // behaviors go here
 });
 
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
 // configure pipeline
 
+app.MapCarter();
 
 
 app.Run();
