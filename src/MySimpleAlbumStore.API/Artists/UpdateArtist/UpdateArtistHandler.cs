b65a2024937bc8bd0046c6d82aa5c7df4e1ab0a6
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-06-07 11:14:43 -0400

    added validation to UpdateArtistHandler

--- src/MySimpleAlbumStore.API/Artists/UpdateArtist/UpdateArtistHandler.cs
+++ src/MySimpleAlbumStore.API/Artists/UpdateArtist/UpdateArtistHandler.cs
@@ -1,25 +1,28 @@
-﻿
+﻿namespace MySimpleAlbumStore.API.Artists.UpdateArtist;
 
-namespace MySimpleAlbumStore.API.Artists.UpdateArtist
+public record UpdateArtistCommand(Guid ArtistId, string Name, ICollection<Album>? Albums) : ICommand<UpdateArtistResult>;
+public record UpdateArtistResult(bool Success);
+public class UpdateArtistCommandValidator : AbstractValidator<UpdateArtistCommand>
 {
-    public record UpdateArtistCommand(Guid ArtistId, string Name, ICollection<Album>? Albums) : IRequest<UpdateArtistResult>;
-    public record UpdateArtistResult(bool Success);
-    public class UpdateArtistCommandHandler(IArtistsRepository artistsRepository) : IRequestHandler<UpdateArtistCommand, UpdateArtistResult>
+    public UpdateArtistCommandValidator()
     {
-        public async Task<UpdateArtistResult> Handle(UpdateArtistCommand request, CancellationToken cancellationToken)
-        {
-            var artistToUpdate = await artistsRepository.GetArtistByIdAsync(request.ArtistId);
-            if (artistToUpdate == null)
-            {
-                throw new NotFoundException($"Artist not found with id: {request.ArtistId}");
-            }
-
-            artistToUpdate.Name = request.Name;
-            artistToUpdate.Albums = request.Albums;
-
-            return new UpdateArtistResult(await artistsRepository.UpdateArtistAsync(artistToUpdate));
-
-        }
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
     }
+}
+public class UpdateArtistCommandHandler(IArtistsRepository artistsRepository) : IRequestHandler<UpdateArtistCommand, UpdateArtistResult>
+{
+    public async Task<UpdateArtistResult> Handle(UpdateArtistCommand request, CancellationToken cancellationToken)
+    {
+        var artistToUpdate = await artistsRepository.GetArtistByIdAsync(request.ArtistId);
+        if (artistToUpdate == null)
+        {
+            throw new NotFoundException($"Artist not found with id: {request.ArtistId}");
+        }
 
+        artistToUpdate.Name = request.Name;
+        artistToUpdate.Albums = request.Albums;
+
+        return new UpdateArtistResult(await artistsRepository.UpdateArtistAsync(artistToUpdate));
+
+    }
 }
